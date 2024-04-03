e758e8be6321029326490619c5b315b1e31ae5dd
bugarela <gabrielamoreira05@gmail.com>
2024-04-03 18:26:30 -0300

    Add CTF contracts as snapshot tests

--- default.nix
+++ default.nix
@@ -2,7 +2,13 @@ with (import <nixpkgs> { });
 let
   shell = mkShell {
     name = "rust-env";
-    buildInputs = [ pkgs.rustup pkgs.rustc pkgs.rust-analyzer pkgs.rustic-rs ];
+    buildInputs = [
+      pkgs.rustup
+      pkgs.rustc
+      pkgs.rust-analyzer
+      pkgs.rustic-rs
+      pkgs.cargo-insta
+    ];
     shellHook = ''
       rustup toolchain add nightly-2024-01-06
       rustup override set nightly-2024-01-06
